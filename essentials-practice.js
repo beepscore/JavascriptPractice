5e2fdc9b1a561bf00c1fe031109354b872597a10
beepscore <support@beepscore.com>
2011-12-24 18:50:19 -0800

    Add function testZeroIsFalse

--- essentials-practice.js
+++ essentials-practice.js
@@ -20,13 +20,38 @@ if (a = 3)
 }
 
 
-if (b = 0)
+function testZeroIsFalse()
 {
-    console.error("b = 0 returns true");
-} else
-{
-    console.info("b = 0 returns false");
+
+    if (!0)
+    {
+        console.debug("testZeroIsFalse pass");
+    } else
+    {
+        console.error("testZeroIsFalse fail");
+    }
+
+    var expectedResult = false;
+    if (0 == expectedResult)
+    {
+        console.debug("testZeroIsFalse pass");
+    } else
+    {
+        console.error("testZeroIsFalse fail");
+    }
+    
+    var myVar;
+    // assignment = not equality ==
+    // after assignment, myVar should be false
+    if ((myVar = 0) == false)
+    {
+        console.debug("testZeroIsFalse pass");
+    } else
+    {
+        console.error("testZeroIsFalse fail");
+    }  
 }
+
 
 
 function testNumber()
@@ -45,20 +70,22 @@ function testNumber()
 
 
 function testNaN()
-{
-    // Number method attempts to explicitly convert argument to a number. It may return NaN.
-    var actualResult = Number("foo");
-    
+{    
     // if assertion fails, assert logs and throws an exception
     // if assertion passes, assert doesn't log
     // Reference:
     // http://getfirebug.com/wiki/index.php/Console_API
+    console.assert(!isNaN(2), "testNaN fail");
+
+    // Number method attempts to explicitly convert argument to a number. It may return NaN.
+    var actualResult = Number("foo");
     console.assert(isNaN(actualResult), "testNaN fail");
-    console.assert(isNaN(2), "testNaN fail");
 }
 
 
 // call test functions
+testZeroIsFalse();
+
 testNumber();
 
 testNaN();
